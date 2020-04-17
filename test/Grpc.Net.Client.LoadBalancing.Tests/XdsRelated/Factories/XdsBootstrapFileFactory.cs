5bb10e668172f2df9bd80445911bf778068b034e
Paweł Wichary <wicharypawel@gmail.com>
2020-04-17 14:29:11 +0200

    Add bootstrapper

---
+++ test/Grpc.Net.Client.LoadBalancing.Tests/XdsRelated/Factories/XdsBootstrapFileFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Grpc.Net.Client.LoadBalancing.Tests.Policies.Factories
+{
+    internal static class XdsBootstrapFileFactory
+    {
+        public static string GetSampleFile(string fileName)
+        {
+            var assemblyPath = Path.GetDirectoryName(Assembly.GetAssembly(typeof(XdsBootstrapFileFactory))!.Location)
+                ?? throw new InvalidOperationException("Assembly location not found");
+            var bootstrapFilePath = Path.Combine(assemblyPath, "XdsRelated", "Factories", fileName);
+            return File.ReadAllText(bootstrapFilePath);
+        }
+    }
+}
