19fb49e141f2c9a97b551b857dbbee02cb5c69c9
Paweł Wichary <wicharypawel@gmail.com>
2020-04-20 21:18:50 +0200

    Add tests to verify override DefaultLoadBalancingPolicy

--- test/Grpc.Net.Client.LoadBalancing.Tests/ResolverPlugins/NoOpResolverPluginTests.cs
+++ test/Grpc.Net.Client.LoadBalancing.Tests/ResolverPlugins/NoOpResolverPluginTests.cs
@@ -1,5 +1,6 @@
 ﻿using Grpc.Net.Client.LoadBalancing.Internal;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -15,12 +16,31 @@ namespace Grpc.Net.Client.LoadBalancing.Tests.ResolverPlugins
 
             // Act
             var resolutionResult = await resolverPlugin.StartNameResolutionAsync(new Uri("https://sample.host.com"));
+            var serviceConfig = resolutionResult.ServiceConfig.Config as GrpcServiceConfig ?? throw new InvalidOperationException("Missing config");
 
             // Assert
             Assert.Single(resolutionResult.HostsAddresses);
             Assert.Equal("sample.host.com", resolutionResult.HostsAddresses[0].Host);
             Assert.Equal(443, resolutionResult.HostsAddresses[0].Port);
             Assert.False(resolutionResult.HostsAddresses[0].IsLoadBalancer);
+            Assert.Single(serviceConfig.RequestedLoadBalancingPolicies);
+            Assert.Equal("pick_first", serviceConfig.RequestedLoadBalancingPolicies[0]);
+        }
+
+        [Fact]
+        public async Task ForOverrideDefaultPolicy_UseNoOpResolverPlugin_ReturnServiceConfigWithOverridenPolicyName()
+        {
+            // Arrange
+            var attributes = new GrpcAttributes(new Dictionary<string, object>() { { GrpcAttributesConstants.DefaultLoadBalancingPolicy, "round_robin" } });
+            var resolverPlugin = new NoOpResolverPlugin(attributes);
+
+            // Act
+            var resolutionResult = await resolverPlugin.StartNameResolutionAsync(new Uri("https://sample.host.com"));
+            var serviceConfig = resolutionResult.ServiceConfig.Config as GrpcServiceConfig ?? throw new InvalidOperationException("Missing config");
+
+            // Assert
+            Assert.Single(serviceConfig.RequestedLoadBalancingPolicies);
+            Assert.Equal("round_robin", serviceConfig.RequestedLoadBalancingPolicies[0]);
         }
 
         [Fact]
