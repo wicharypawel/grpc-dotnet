4e78e421ea299d9571f1fbe43e425c326c504b1b
Paweł Wichary <wicharypawel@gmail.com>
2020-05-11 13:10:19 +0200

    Add new GrpcPickResult

---
+++ src/Grpc.Net.Client/LoadBalancing/GrpcPickResult.cs
@@ -0,0 +1,86 @@
+﻿using Grpc.Core;
+using System;
+
+namespace Grpc.Net.Client.LoadBalancing
+{
+    /// <summary>
+    /// A balancing decision made by picker for an RPC.
+    /// </summary>
+    public sealed class GrpcPickResult
+    {
+        private static readonly GrpcPickResult NoResult = new GrpcPickResult(null, Status.DefaultSuccess, false);
+
+        /// <summary>
+        /// The Subchannel if this result was created by <see cref="WithSubChannel"/>, or null otherwise.
+        /// </summary>
+        public GrpcSubChannel? SubChannel { get; }
+
+        /// <summary>
+        /// The status associated with this result. Non-OK if created using <see cref="WithError"/> method or <see cref="Status.DefaultSuccess"/> otherwise.
+        /// </summary>                      
+        public Status Status { get; }
+
+        /// <summary>
+        /// Returns true if this result was created by <see cref="WithDrop"/> method.
+        /// </summary>
+        public bool Drop { get; }
+
+        private GrpcPickResult(GrpcSubChannel? subChannel, Status status, bool drop)
+        {
+            SubChannel = subChannel;
+            Status = status;
+            Drop = drop;
+        }
+
+        /// <summary>
+        /// A decision to proceed the RPC on a Subchannel.
+        /// </summary>
+        /// <param name="subChannel">Subchannel the picked Subchannel.</param>
+        /// <returns>A balancing decision.</returns>
+        public static GrpcPickResult WithSubChannel(GrpcSubChannel subChannel)
+        {
+            if (subChannel == null)
+            {
+                throw new ArgumentNullException(nameof(subChannel));
+            }
+            return new GrpcPickResult(subChannel, Status.DefaultSuccess, false);
+        }
+
+        /// <summary>
+        /// A decision to report a connectivity error to the RPC.
+        /// </summary>
+        /// <param name="error">Error the error status. Must not be OK.</param>
+        /// <returns>A balancing decision.</returns>
+        public static GrpcPickResult WithError(Status error)
+        {
+            if (error.StatusCode == StatusCode.OK)
+            {
+                throw new ArgumentException("Error status shouldn't be OK.");
+            }
+            return new GrpcPickResult(null, error, false);
+        }
+
+        /// <summary>
+        /// A decision to fail an RPC immediately.  This is a final decision and will ignore retry policy.
+        /// </summary>
+        /// <param name="status">Status the status with which the RPC will fail. Must not be OK.</param>
+        /// <returns>A balancing decision.</returns>
+        public static GrpcPickResult WithDrop(Status status)
+        {
+            if (status.StatusCode == StatusCode.OK)
+            {
+                throw new ArgumentException("Drop status shouldn't be OK.");
+            }
+            return new GrpcPickResult(null, status, true);
+        }
+
+        /// <summary>
+        /// No decision could be made.  The RPC will stay buffered.
+        /// </summary>
+        /// <returns>A balancing decision.</returns>
+        public static GrpcPickResult WithNoResult()
+        {
+            return NoResult;
+        }
+    }
+}
---
+++ test/Grpc.Net.Client.LoadBalancing.Tests/Core/GrpcPickResultTests.cs
@@ -0,0 +1,67 @@
+﻿using Grpc.Core;
+using System;
+using Xunit;
+
+namespace Grpc.Net.Client.LoadBalancing.Tests.Core
+{
+    public sealed class GrpcPickResultTests
+    {
+        [Fact]
+        public void ForSampleChannel_UsingPickResultWithSubChannel_ReturnsWrappedChannel()
+        {
+            // Arrange
+            var subChannel = new GrpcSubChannel(new Uri("http://10.1.5.210:80"));
+
+            // Act
+            var pickResult = GrpcPickResult.WithSubChannel(subChannel);
+
+            // Assert
+            Assert.Equal(subChannel, pickResult.SubChannel);
+            Assert.Equal(Status.DefaultSuccess, pickResult.Status);
+            Assert.False(pickResult.Drop);
+        }
+
+        [Fact]
+        public void ForNoResult_UsingPickResultWithNoResult_ReturnsNoResultAndNullChannel()
+        {
+            // Arrange
+            // Act
+            var pickResult = GrpcPickResult.WithNoResult();
+
+            // Assert
+            Assert.Null(pickResult.SubChannel);
+            Assert.Equal(Status.DefaultSuccess, pickResult.Status);
+            Assert.False(pickResult.Drop);
+        }
+
+        [Fact]
+        public void ForErrorStatus_UsingPickResultWithError_ReturnsErrorAndNullChannel()
+        {
+            // Arrange
+            var status = new Status(StatusCode.Unavailable, "for test");
+
+            // Act
+            var pickResult = GrpcPickResult.WithError(status);
+
+            // Assert
+            Assert.Null(pickResult.SubChannel);
+            Assert.Equal(status, pickResult.Status);
+            Assert.False(pickResult.Drop);
+        }
+
+        [Fact]
+        public void ForErrorStatus_UsingPickResultWithDrop_ReturnsWrappedChannel()
+        {
+            // Arrange
+            var status = new Status(StatusCode.Unavailable, "for test");
+
+            // Act
+            var pickResult = GrpcPickResult.WithDrop(status);
+
+            // Assert
+            Assert.Null(pickResult.SubChannel);
+            Assert.Equal(status, pickResult.Status);
+            Assert.True(pickResult.Drop);
+        }
+    }
+}
