2a697a4bcbce4f91c42b7456d8e885d8a832a7d4
Paweł Wichary <wicharypawel@gmail.com>
2020-05-14 14:33:41 +0200

    Add InterlockedBool implementation

---
+++ src/Grpc.Net.Client/Internal/InterlockedBool.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading;
+
+namespace Grpc.Net.Client.Internal
+{
+    /// <summary>
+    /// A bool value that may be updated atomically.
+    /// 
+    /// This class was created because dotnet does not support <see cref="Interlocked"/> for bool.
+    /// See more: https://github.com/dotnet/runtime/issues/4282
+    /// </summary>
+    internal sealed class InterlockedBool
+    {
+        private const int FALSE = 0;
+        private const int TRUE = 1;
+        private int _value;
+
+        /// <summary>
+        /// Creates a new <see cref="InterlockedBool"/> with initial value false.
+        /// </summary>
+        public InterlockedBool()
+        {
+            _value = FALSE;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="InterlockedBool"/> with the given initial value.
+        /// </summary>
+        /// <param name="initialValue">Initial value.</param>
+        public InterlockedBool(bool initialValue)
+        {
+            _value = initialValue ? TRUE : FALSE;
+        }
+
+        /// <summary>
+        /// Atomically sets the value to the given updated value if the current value == the expected value.
+        /// Returns true if successful. False return indicates that the actual value was not equal to the expected value.
+        /// </summary>
+        public bool CompareAndSet(bool expect, bool update)
+        {
+            var originalValue = Interlocked.CompareExchange(ref _value, update ? TRUE : FALSE, expect ? TRUE : FALSE) == TRUE;
+            return originalValue == expect;
+        }
+
+        /// <summary>
+        /// Returns the current value.
+        /// </summary>
+        public bool Get()
+        {
+            return _value == TRUE;
+        }
+
+        /// <summary>
+        /// Atomically sets to the given value and returns the previous value.
+        /// </summary>
+        public bool GetAndSet(bool newValue)
+        {
+            return Interlocked.Exchange(ref _value, newValue ? TRUE : FALSE) == TRUE;
+        }
+
+        /// <summary>
+        /// Unconditionally sets to the given value.
+        /// </summary>
+        public void Set(bool newValue)
+        {
+            GetAndSet(newValue);
+        }
+    }
+}
---
+++ test/Grpc.Net.Client.LoadBalancing.Tests/Core/InterlockedBoolTests.cs
@@ -0,0 +1,66 @@
+﻿using Grpc.Net.Client.Internal;
+using Xunit;
+
+namespace Grpc.Net.Client.LoadBalancing.Tests.Core
+{
+    public sealed class InterlockedBoolTests
+    {
+        [Fact]
+        public void ForVerifyInitialization_UsingInterlockedBool()
+        {
+            // Arrange
+            // Act
+            // Assert
+            Assert.False(new InterlockedBool().Get());
+            Assert.True(new InterlockedBool(true).Get());
+            Assert.False(new InterlockedBool(false).Get());
+        }
+
+        [Fact]
+        public void ForVerifySetValue_UsingInterlockedBool()
+        {
+            // Arrange
+            var value = new InterlockedBool(false);
+            
+            // Act
+            value.Set(true);
+            Assert.True(value.Get());
+            value.Set(false);
+
+            // Assert
+            Assert.False(value.Get());
+        }
+
+        [Fact]
+        public void ForVerifyGetAndSet_UsingInterlockedBool()
+        {
+            // Arrange
+            var value = new InterlockedBool(false);
+
+            // Act
+            var previusValue = value.GetAndSet(true);
+
+            // Assert
+            Assert.False(previusValue);
+            Assert.True(value.Get());
+        }
+
+        [Theory]
+        [InlineData(false, true, true, true)]
+        [InlineData(true, false, false, false)]
+        [InlineData(false, false, true, false)]
+        [InlineData(true, true, false, false)]
+        public void ForVerifyCompareAndSet_UsingInterlockedBool(bool expect, bool update, bool changed, bool final)
+        {
+            // Arrange
+            var value = new InterlockedBool(false);
+
+            // Act
+            var changeSuccess = value.CompareAndSet(expect, update);
+
+            // Assert
+            Assert.Equal(changed, changeSuccess);
+            Assert.Equal(final, value.Get());
+        }
+    }
+}
