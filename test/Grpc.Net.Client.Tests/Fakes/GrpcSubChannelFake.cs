41d906b38027a4a72e5cd7a657e10e29e023b591
Paweł Wichary <wicharypawel@gmail.com>
2020-05-22 10:13:44 +0200

    Add Fake for tests to allow compilation

---
+++ test/Grpc.Net.Client.Tests/Fakes/GrpcSubChannelFake.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Net.Client.LoadBalancing;
+using System;
+
+namespace Grpc.Net.Client.Tests.Fakes
+{
+    internal sealed class GrpcSubChannelFake : IGrpcSubChannel
+    {
+        public Uri Address { get; set; }
+
+        public GrpcAttributes Attributes { get; set; }
+
+        public GrpcSubChannelFake(Uri address, GrpcAttributes attributes)
+        {
+            Address = address;
+            Attributes = attributes;
+        }
+
+        public void RequestConnection()
+        {
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public void Start(IGrpcSubchannelStateObserver observer)
+        {
+        }
+
+        public void UpdateAddress(Uri address)
+        {
+        }
+    }
+}
--- test/Grpc.Net.Client.Tests/HttpContentClientStreamReaderTests.cs
+++ test/Grpc.Net.Client.Tests/HttpContentClientStreamReaderTests.cs
@@ -26,6 +26,7 @@ using Greet;
 using Grpc.Core;
 using Grpc.Net.Client.Internal;
 using Grpc.Net.Client.LoadBalancing;
+using Grpc.Net.Client.Tests.Fakes;
 using Grpc.Tests.Shared;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Testing;
@@ -210,7 +211,7 @@ namespace Grpc.Net.Client.Tests
                 ClientTestHelpers.ServiceMethod,
                 new GrpcMethodInfo(new GrpcCallScope(ClientTestHelpers.ServiceMethod.Type, uri), uri),
                 new CallOptions(),
-                channel, new GrpcSubChannelFake());
+                channel, new GrpcSubChannelFake(uri, GrpcAttributes.Empty));
         }
 
         private static GrpcChannel CreateChannel(HttpClient httpClient, ILoggerFactory? loggerFactory = null, bool? throwOperationCanceledOnCancellation = null)
@@ -223,25 +224,6 @@ namespace Grpc.Net.Client.Tests
                     LoggerFactory = loggerFactory,
                     ThrowOperationCanceledOnCancellation = throwOperationCanceledOnCancellation ?? false
                 });
-        }
-    }
-
-    internal sealed class GrpcSubChannelFake : IGrpcSubChannel
-    {
-        public Uri Address => throw new NotImplementedException();
-
-        public GrpcAttributes Attributes => GrpcAttributes.Empty;
-
-        public void RequestConnection()
-        {
-        }
-
-        public void Shutdown()
-        {
-        }
-
-        public void Start(IGrpcSubchannelStateObserver observer)
-        {
         }
     }
 }
