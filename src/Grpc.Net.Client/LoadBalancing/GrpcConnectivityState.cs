e8696e8b601d1f0a1aacb77861f87b7240465af2
Paweł Wichary <wicharypawel@gmail.com>
2020-05-22 10:53:45 +0200

    Add Copyright info to src

--- src/Grpc.Net.Client/LoadBalancing/GrpcConnectivityState.cs
+++ src/Grpc.Net.Client/LoadBalancing/GrpcConnectivityState.cs
@@ -1,4 +1,22 @@
-﻿//This enum is not related to load balancing but rather to general gRPC channel implementation.
+﻿#region Copyright notice and license
+
+// Copyright 2019 The gRPC Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#endregion
+
+//This enum is not related to load balancing but rather to general gRPC channel implementation.
 //In order to make is easier for future and moving file to better position in file tree it has namespace not related to loadbalancing.
 namespace Grpc.Net.Client
 {
