8bb74ab6e94e9e5a41b85e45bd8630d02aa16424
Paweł Wichary <wicharypawel@gmail.com>
2020-06-09 14:38:57 +0200

    Refactor rename field in GrpcNameResolutionObserver

--- src/Grpc.Net.Client/LoadBalancing/Internal/GrpcNameResolutionObserver.cs
+++ src/Grpc.Net.Client/LoadBalancing/Internal/GrpcNameResolutionObserver.cs
@@ -24,15 +24,15 @@ namespace Grpc.Net.Client.LoadBalancing.Internal
 {
     internal sealed class GrpcNameResolutionObserver : IGrpcNameResolutionObserver
     {
-        private readonly GrpcChannel _grpcChannel;
+        private readonly GrpcChannel _channel;
         private readonly IGrpcHelper _helper;
         private readonly ILogger _logger;
 
-        public GrpcNameResolutionObserver(GrpcChannel grpcChannel, IGrpcHelper helper)
+        public GrpcNameResolutionObserver(GrpcChannel channel, IGrpcHelper helper)
         {
-            _grpcChannel = grpcChannel ?? throw new ArgumentNullException(nameof(grpcChannel));
+            _channel = channel ?? throw new ArgumentNullException(nameof(channel));
             _helper = helper ?? throw new ArgumentNullException(nameof(helper));
-            _logger = grpcChannel.LoggerFactory.CreateLogger<GrpcNameResolutionObserver>();
+            _logger = channel.LoggerFactory.CreateLogger<GrpcNameResolutionObserver>();
         }
 
         public void OnNext(GrpcNameResolutionResult value)
@@ -40,15 +40,15 @@ namespace Grpc.Net.Client.LoadBalancing.Internal
             _helper.GetSynchronizationContext().Execute(() =>
             {
                 _logger.LogDebug("Name resolution results received.");
-                if (_grpcChannel.LastResolutionState != GrpcResolutionState.Success)
+                if (_channel.LastResolutionState != GrpcResolutionState.Success)
                 {
-                    _grpcChannel.LastResolutionState = GrpcResolutionState.Success;
+                    _channel.LastResolutionState = GrpcResolutionState.Success;
                 }
-                _grpcChannel.NameResolverRefreshBackoffPolicy = null;
+                _channel.NameResolverRefreshBackoffPolicy = null;
                 var effectiveServiceConfig = value.ServiceConfig.Config ?? new object();
                 var resolvedAddresses = new GrpcResolvedAddresses(value.HostsAddresses, effectiveServiceConfig, value.Attributes);
-                if (_helper != _grpcChannel.Helper) return;
-                Status handleResult = _grpcChannel.TryHandleResolvedAddresses(resolvedAddresses);
+                if (_helper != _channel.Helper) return;
+                Status handleResult = _channel.TryHandleResolvedAddresses(resolvedAddresses);
                 if (handleResult.StatusCode != StatusCode.OK)
                 {
                     HandleErrorInSyncContext(handleResult);
@@ -71,36 +71,36 @@ namespace Grpc.Net.Client.LoadBalancing.Internal
 
         private void HandleErrorInSyncContext(Status error)
         {
-            if (_grpcChannel.LastResolutionState != GrpcResolutionState.Error)
+            if (_channel.LastResolutionState != GrpcResolutionState.Error)
             {
-                _grpcChannel.LastResolutionState = GrpcResolutionState.Error;
+                _channel.LastResolutionState = GrpcResolutionState.Error;
             }
             // Call LB only if it's not shutdown. If LB is shutdown, lbHelper won't match. This is required for idle mode implementation.
-            if (_helper != _grpcChannel.Helper) return;
-            _grpcChannel.HandleNameResolutionError(error);
+            if (_helper != _channel.Helper) return;
+            _channel.HandleNameResolutionError(error);
             ScheduleExponentialBackOffInSyncContext();
         }
 
         /// Think about moving this method to <see cref="GrpcChannel"/>.
         private void ScheduleExponentialBackOffInSyncContext()
         {
-            _grpcChannel.SyncContext.ThrowIfNotInThisSynchronizationContext();
-            if (_grpcChannel.NameResolverRefreshSchedule?.IsPending() ?? false)
+            _channel.SyncContext.ThrowIfNotInThisSynchronizationContext();
+            if (_channel.NameResolverRefreshSchedule?.IsPending() ?? false)
             {
                 // The name resolver may invoke onError multiple times, but we only want to
                 // schedule one backoff attempt.
                 return;
             }
-            if (_grpcChannel.NameResolverRefreshBackoffPolicy == null)
+            if (_channel.NameResolverRefreshBackoffPolicy == null)
             {
-                _grpcChannel.NameResolverRefreshBackoffPolicy = _grpcChannel.BackoffPolicyProvider.CreateBackoffPolicy();
+                _channel.NameResolverRefreshBackoffPolicy = _channel.BackoffPolicyProvider.CreateBackoffPolicy();
             }
-            var delay = _grpcChannel.NameResolverRefreshBackoffPolicy.NextBackoff();
+            var delay = _channel.NameResolverRefreshBackoffPolicy.NextBackoff();
             _logger.LogDebug($"Scheduling name resolution backoff for {delay}.");
-            _grpcChannel.NameResolverRefreshSchedule = _helper.GetSynchronizationContext().Schedule(() =>
+            _channel.NameResolverRefreshSchedule = _helper.GetSynchronizationContext().Schedule(() =>
             {
-                _grpcChannel.NameResolverRefreshSchedule = null;
-                _grpcChannel.RefreshNameResolution();
+                _channel.NameResolverRefreshSchedule = null;
+                _channel.RefreshNameResolution();
             }, delay);
         }
     }
